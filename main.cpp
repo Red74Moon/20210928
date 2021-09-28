51e691b40bb189c78f74e8afe5e33e30d9208749
Red74Moon <fedoormaster@gmail.com>
2021-09-28 10:07:48 +0900

    기본 틀 작성

--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
+#include <windows.h> //windows 헤더파일 include
 
 using namespace std;
 
+void Input();
+void Process();
+void Render();
+
+bool bGmaeState = true;
+
+char Key;
+
 int main()
 { 
-	// 끝이 없이 계속 반복 될때
-	while (true)
+	// -- Game Engine이 하는 일 --
+	// 
+	// 사용자의 입력을 받는다. (Input)
+	// 물체를 배치(수학계산)한다. (Process) 
+	// 그림을 그린다.(가상공간에서 사진 찍는다.) (Render)
+	// 
+	//
+	while (bGmaeState)
 	{ 
-		cout << "Hello World" << endl;
+		Input();
+		Process();
+		Render();
 	}
 
 	return 0;
 }
 
+void Input()
+{
+	cin >> Key;
+}
+
+void Process()
+{
+	if (Key == 'q' || Key == 'Q')
+	{
+		bGmaeState = false;
+	}
+}
+
+void Render()
+{
+	system("cls"); //콘솔창 clear
+	cout << "그린다." << endl;
+}
